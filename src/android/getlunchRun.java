1f5b7587c2be1019e950e1c101274b2fca9988e0
PavelDubrovskiy <freecrus@gmail.com>
2016-04-07 13:16:35 +0300

    Updated 13.16 07.04.2016

--- src/android/getlunchRun.java
+++ src/android/getlunchRun.java
@@ -7,8 +7,8 @@
 */
 package ru.getlunch.run;
 
-import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONException;
