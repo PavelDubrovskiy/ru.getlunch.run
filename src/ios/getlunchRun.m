d033afb77b1751db9fbcbdb31bfe0aa084056171
PavelDubrovskiy <freecrus@gmail.com>
2016-04-29 11:27:05 +0300

    Updated 11.27 29.04.2016

--- src/ios/getlunchRun.m
+++ src/ios/getlunchRun.m
@@ -8,18 +8,18 @@
     CDVPluginResult *pluginResult = nil;
     NSString *scheme = [command.arguments objectAtIndex:0];
 	NSString *result = [NSString stringWithFormat:@"%@%@", @"yandexnavi://build_route_on_map?", scheme];
+	
+	result = [result stringByReplacingOccurrencesOfString:@"\"" withString:@""];
+	
 	NSURL *naviURL = [NSURL URLWithString:result];
 	
 	if ([[UIApplication sharedApplication] canOpenURL:naviURL]) {
-		// Если Навигатор установлен - открываем его
 		[[UIApplication sharedApplication] openURL:naviURL];
-		//pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:(true)];
 	} else {
-		// Если не установлен - открываем страницу в App Store
 		NSURL *appStoreURL = [NSURL URLWithString:@"https://itunes.apple.com/ru/app/yandeks.navigator/id474500851?mt=8"];
 		[[UIApplication sharedApplication] openURL:appStoreURL];
-		//pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsBool:(false)];
 	}
+	
 	pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
 	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
@@ -38,7 +38,6 @@
 	NSURL *naviURL = [NSURL URLWithString:result];
 	
 	[[UIApplication sharedApplication] openURL:naviURL];
-	//pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:(true)];
 	pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
 	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
